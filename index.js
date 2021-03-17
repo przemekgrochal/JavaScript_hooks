7d54a685ebe59340a05abaa88810358e5cd36977
przemekgrochal <45077067+przemekgrochal@users.noreply.github.com>
2021-03-17 13:17:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Sortowanie tablicy ze sprawdzeniem wielkości liter
-const array = [99,45,2.45,1,1.11,2.12,0, 'ania', 'Zygmunt', -1, 'Beata', 'diana', 'Celina'];
+const array = [99,13, 18, 100, 66, 45,2.45,1,1.11,2.12,0, 'ania', 23.45, 'Zygmunt', -1, 'Beata', 'diana', 'Celina'];
 
 const check = (item) => {
   if(typeof item === 'string') {
@@ -15,4 +15,4 @@ const newArray = array.map(item => {
 	return check(item);
 })
 
-console.log(newArray.sort());
+console.log(newArray.sort((a, b) => a - b));
